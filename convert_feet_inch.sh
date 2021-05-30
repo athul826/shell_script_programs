67576e3e0bba13625619634425ee34d7ab8f7311
athul826 <athultharol1994@gmail.com>
2021-05-30 18:53:36 +0530

    ADDED: adddice.sh convert_feet_inch.sh dice.sh five_random_two_digit.sh rectangular_plotof_60_40.sh single_digit.sh

---
+++ convert_feet_inch.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+inch=42;
+feet=$(($inch/12))
+echo "42 inch in feet is ="$feet
+
---
+++ rectangular_plotof_60_40.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+length=60;
+breadth=40;
+Length=$(($length/3))
+Breadth=$(($breadth/3))
+echo "Rectangular plot in meter is "$Length "meter  "$Breadth "meter"
